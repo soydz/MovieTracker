59601e458250e91524865977298f9c18f384c24a
soydz <dev@soydz.com>
2025-03-30 16:10:38 -0500

    chore(Root): Creación y configuración inicial del proyecto, puesta en marcha de Spring Boot y enlace base de datos PostgreSQL

---
+++ Backend/src/main/java/com/soydz/estructuraDatos/App.java
@@ -0,0 +1,12 @@
+package com.soydz.estructuraDatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
